ca86a0e2ed3cfb56921d4a5aa7ef5a2d7f1ba5b0
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2018-11-11 22:18:39 -0800

    Update REDDITBOT2.py

--- REDDITBOT2.py
+++ REDDITBOT2.py
@@ -14,7 +14,7 @@ mcLength = 0;
 mc = bmemcached.Client(os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
      os.environ.get('MEMCACHEDCLOUD_USERNAME'),
      os.environ.get('MEMCACHEDCLOUD_PASSWORD'))
-mc.set("postIDs", postIDs)
+mc.get("postIDs")
 reddit = praw.Reddit(client_id=os.environ['ID'],
                      client_secret=os.environ['SECRET'],
                      username=os.environ['REDDIT_USERNAME'],
