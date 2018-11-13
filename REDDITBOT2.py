c93e32588a3d49c684ea34850948b37fdc5c0b7e
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2018-11-12 17:24:47 -0800

    Update REDDITBOT2.py

--- REDDITBOT2.py
+++ REDDITBOT2.py
@@ -1,5 +1,5 @@
 import praw
-import urbandictionary as ud
+from urbandictionary import urbandictionary as ud
 from psaw import PushshiftAPI
 from nltk.corpus import wordnet as wn
 from bs4 import BeautifulSoup
