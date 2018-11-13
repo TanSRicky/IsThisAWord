97f09fb61edd9aa9a887d3a955f46d02a8341668
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2018-11-12 17:23:58 -0800

    Add files via upload

---
+++ urbandictionary/setup.py
@@ -0,0 +1,21 @@
+import multiprocessing
+from setuptools import setup
+
+setup(name='urbandictionary',
+    version='1.1',
+    py_modules=['urbandictionary'],
+    author='Bocong',
+    author_email='bocongyin@gmail.com',
+    license='MIT',
+    description='Simple Python wrapper for Urban Dictionary API.',
+    url='https://github.com/bocong/urbandictionary-py',
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ],
+    keywords='urban dictionary python api wrapper',
+    )
