6816d9e1e135b321e189d136bc4e2cff3a1dc2a2
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2018-11-15 19:07:15 -0800

    Update REDDITBOT2.py

--- REDDITBOT2.py
+++ REDDITBOT2.py
@@ -5,12 +5,12 @@ from nltk.corpus import wordnet as wn
 from bs4 import BeautifulSoup
 import bmemcached
 import os
-
+from wordnik import *
 
 
 api = PushshiftAPI()
 mcLength = 0;
-#client = swagger.ApiClient(os.environ('wnKEY'), os.environ('wnURL'))
+client = swagger.ApiClient(os.environ('wnKEY'), os.environ('wnURL'))
 mc = bmemcached.Client(os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
      os.environ.get('MEMCACHEDCLOUD_USERNAME'),
      os.environ.get('MEMCACHEDCLOUD_PASSWORD'))
